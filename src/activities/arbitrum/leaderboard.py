fee7f5b547e0f57bb6f31f11681e830a7d4a2a08
alex <alexander.grablevski@gmail.com>
2023-11-18 23:14:46 +0300

    Add lb

--- src/activities/arbitrum/leaderboard.py
+++ src/activities/arbitrum/leaderboard.py
@@ -1,2 +1,28 @@
 def get_lb():
-    return [1, 2, 3]
+    return [
+        {
+            "Address": "0xbf81c9bf9883abab878952fcbfb84914a67b6e43",
+            "Program Engegement": 102.53,
+            "Protocol Activity": 10.09,
+            "Competitors Activity": 16.01,
+            "Sybil Likelihood": 14.23,
+            "Total XP": 142.86
+        },
+        {
+            "Address": "0x0dc7fc49f79b310b3ea8919ea15429d35c565d11",
+            "Program Engegement": 216.23,
+            "Protocol Activity": 25.19,
+            "Competitors Activity": 3.14,
+            "Sybil Likelihood": 21.22,
+            "Total XP": 265.78
+        },
+        {
+            "Address": "0x54d38638060f6ccaa451b5e19ab720e3f1bb125e",
+            "Program Engegement": 80.10,
+            "Protocol Activity": 18.54,
+            "Competitors Activity": 7.00,
+            "Sybil Likelihood": 35.25,
+            "Total XP": 140.89
+        },
+    ]
+
