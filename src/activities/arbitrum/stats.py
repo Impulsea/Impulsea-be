9a9457b854bc8bcf34311c01bd626836efd6d3b9
alex <alexander.grablevski@gmail.com>
2023-12-09 16:40:21 +0300

    Update retention

--- src/activities/arbitrum/stats.py
+++ src/activities/arbitrum/stats.py
@@ -1,6 +1,6 @@
 def get_stats():
     return {
-        'retention': 0.42,
+        'retention': 14.9563065501508,
         'stikiness': 5.508081306838993,
         'high_engaged': 5496,
         'med_engaged': 28412,
